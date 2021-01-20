11870d2e46448e026d5eb404621aa3a3d554fa85
Jake Butler <tjakebutler@gmail.com>
2021-01-20 15:21:57 -0600

    updated the get_single_entry function into the request.py of the entries module, tested in post man and returning single GET requests with targets id

--- entries/request.py
+++ entries/request.py
@@ -3,6 +3,37 @@ import json
 from models import Entry
 
 
+def get_single_entry(id):
+    with sqlite3.connect("./dailyjournal.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.date,
+            e.concept,
+            e.entry,
+            e.moodID
+        FROM entries e
+        WHERE e.id = ?
+        """, ( id, ))
+
+
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+
+        # Create an animal instance from the current row
+        entry = Entry(data['id'], data['date'], data['concept'],
+                            data['entry'], data['moodID'])
+
+        return json.dumps(entry.__dict__)
+
+
+
+
 def get_all_entries():
     # Open a connection to the database
     with sqlite3.connect("./dailyjournal.db") as conn:
